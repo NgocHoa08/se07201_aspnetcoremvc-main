1b5a94594c4b2a298ba49ab997e81276535370e2
NgocHoa08 <hoa2282005hhh@gmail.com>
2025-12-07 20:40:20 +0700

    Delete SE07201.Tests directory

--- SE07201.Tests/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SE07201.Tests.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
